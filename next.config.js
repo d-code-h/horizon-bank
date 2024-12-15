ddced371831fadbdce48f915a31f86ad998baed0
Yunus Habeeb <habeebdh1@gmail.com>
2024-12-15 06:35:03 +0100

    Downgrarde to Next14 and React18

---
+++ next.config.js
@@ -0,0 +1,35 @@
+// Importing the Sentry configuration (assuming this is the default setup)
+import { withSentryConfig } from '@sentry/nextjs';
+
+const nextConfig = {
+  // Your Next.js configuration options here
+};
+
+export default withSentryConfig(nextConfig, {
+  org: 'techyforge',
+  project: 'horizon',
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors
+  automaticVercelMonitors: true,
+});
